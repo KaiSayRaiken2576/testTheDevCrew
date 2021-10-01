34d90864b6c7a644ba26e5ecf211f6a396b2e17e
Kai <kuiqquke@gmail.com>
2021-10-01 05:51:08 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.sass'
+
+export const App = () => {
+  const a = 13
+
+  return (
+    <div className="App">
+      <header className="App-header">Learn React {a}</header>
+    </div>
+  )
+}
